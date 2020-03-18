fdab70b39a8302a2a38bf56715b8c7fca16ae73f
Qant1 <51907758+Quentin8@users.noreply.github.com>
2020-03-18 15:26:26 +0100

    commit 3

---
+++ Window2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Utilisateur on 18/03/2020.
+//
+
+#include "Window2.h"
---
+++ Window2.h
@@ -0,0 +1,14 @@
+//
+// Created by Utilisateur on 18/03/2020.
+//
+
+#ifndef QT_TP3_WINDOW2_H
+#define QT_TP3_WINDOW2_H
+
+
+class Window2 {
+
+};
+
+
+#endif //QT_TP3_WINDOW2_H
