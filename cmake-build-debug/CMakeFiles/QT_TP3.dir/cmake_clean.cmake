94a7adb7bd7277269129537ad93f87026ff783ac
Qant1 <51907758+Quentin8@users.noreply.github.com>
2020-03-18 14:26:08 +0100

    commit 1

---
+++ cmake-build-debug/CMakeFiles/QT_TP3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "QT_TP3_automoc.cpp"
+  "CMakeFiles/QT_TP3.dir/main.cpp.o"
+  "CMakeFiles/QT_TP3.dir/QT_TP3_automoc.cpp.o"
+  "QT_TP3.pdb"
+  "QT_TP3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QT_TP3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
